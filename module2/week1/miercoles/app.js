005b68ce0d9fa62bcb4289ed0099e03dcaa47c11
Dylan <dyl.blink.1.8.2@gmail.com>
2022-05-25 22:11:06 -0500

    npm package

---
+++ module2/week1/miercoles/app.js
@@ -0,0 +1,9 @@
+//todo lo que importemos va de la linea 1 hacia abajo
+// destructuracion y asignacion 
+const { color, log } = require('console-log-colors');
+//color = red green cyan
+// red() // color.red()
+const { red, green, cyan } = color;
+
+console.log( red("Esto es otro mensjae pero mas pro con color") )
+console.log("Hola mundo")
