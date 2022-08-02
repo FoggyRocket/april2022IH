88cd2d2389c99737e613a49e6c832d0d4970d2b9
Dylan <dyl.blink.1.8.2@gmail.com>
2022-08-02 10:04:58 -0500

    lab routing

---
+++ module3/week3/lunes/lab-wiki-countries/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import { CountriesList, Navbar } from './components'
+import RootRoutes from './RootRoutes';
+import countriesJson from './countries.json'
+function App() {
+  return (
+    //<BrowserRouter>
+    <div className="App">
+      <Navbar/>
+      <div className='container'>
+          <div className='row'>
+              <CountriesList countries={countriesJson}/>
+              {/**La ruta nested */}
+              <RootRoutes/>
+          </div>
+      </div>
+    </div>
+    //<BrowserRouter>
+  );
+}
+
+export default App;
